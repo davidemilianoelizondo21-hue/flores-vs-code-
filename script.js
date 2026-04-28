958c76913d63c5169eb3d0cb75ba34fe5098d827
david emiliano <davidemilianoelizondo21@gmail.com>
2026-04-27 23:50:23 -0600

    nuevo comiit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+// Función para abrir la carta inicial
 function openLetter() {
     const envelope = document.getElementById('envelope-layer');
     envelope.classList.add('opened');
 }
 
+// Función para que el botón NO huya
 function moveNoButton() {
     const btn = document.getElementById('no-btn');
     
-    // Generar posiciones aleatorias para que el botón "huya"
-    // Los números limitan el movimiento dentro del cuadro principal
+    // Generar posiciones aleatorias dentro del cuadro principal
     const newX = Math.random() * (300 - 50) + 10;
     const newY = Math.random() * (200 - 50) + 10;
     
@@ -15,6 +16,7 @@ function moveNoButton() {
     btn.style.top = `${newY}px`;
 }
 
+// Función para mostrar la carta final al presionar SÍ
 function showFinal() {
     const finalLetter = document.getElementById('final-letter');
     finalLetter.style.display = 'block';
